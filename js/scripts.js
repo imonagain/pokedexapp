a78640a8683f4a26a7db4227ccff60a08f27af42
imonagain <101617596+imonagain@users.noreply.github.com>
2022-04-23 08:58:34 -0700

    Updated list of pokemon

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,58 @@
-alert('Hello world');
-
-let faveFood = 'Ice Cream';
-document.write(faveFood);
+let pokemonList = [
+    {
+        name: 'Pikachu',
+        nationalNumber: 25,
+        height: 0.4,
+        type: 'Electric',
+        abilities: 'Static'
+    },
+    {
+        name: 'Staryu',
+        nationalNumber: 120,
+        height: 0.8,
+        type: 'Water',
+        abilities: ['Illuminate', 'Natural Cure']
+    },
+    {
+        name: 'Meowth',
+        nationalNumber: 52,
+        height: 0.4,
+        type: 'Normal',
+        abilities: ['Pickup', 'Technician']
+    },
+    {
+        name: 'Poliwag',
+        nationalNumber: 60,
+        height: 0.6,
+        type: 'Water',
+        abilities: ['Water Absorb', 'Damp']
+    },
+    {
+        name: 'Jigglypuff',
+        nationalNumber: 39,
+        height: 0.5,
+        type: ['Normal', 'Fairy'],
+        abilities: ['Pickup', 'Technician']
+    },
+    {
+        name: 'Cubone',
+        nationalNumber: 104,
+        height: 0.4,
+        type: 'Ground',
+        abilities: ['Rock Head', 'Lightning Rod']
+    },
+    {
+        name: 'Exeggutor',
+        nationalNumber: 103,
+        height: 2,
+        type: ['Grass', 'Psychic'],
+        abilities: 'Chlorophyll'
+    },
+    {
+        name: 'Mewtwo',
+        nationalNumber: 150,
+        height: 2,
+        type: 'Psychic',
+        abilities: 'Pressure'
+    }
+]
