1542ba672862b14ad6f0a5e6f4b8b33211bacf5f
imonagain <101617596+imonagain@users.noreply.github.com>
2022-04-27 01:30:57 -0700

    Finished exercise 1.5

--- js/scripts.js
+++ js/scripts.js
@@ -1,67 +1,131 @@
-let pokemonList = [
+let pokemonListRepository = (function () {
+  let pokemonList = [
     {
-        name: 'Pikachu',
-        nationalNumber: 025,
-        height: 0.4,
-        type: 'Electric',
-        abilities: 'Static'
+      name: "Pikachu",
+      nationalNumber: 025,
+      height: 0.4,
+      type: "Electric",
+      abilities: "Static",
     },
     {
-        name: 'Staryu',
-        nationalNumber: 120,
-        height: 0.8,
-        type: 'Water',
-        abilities: ['Illuminate', 'Natural Cure']
+      name: "Staryu",
+      nationalNumber: 120,
+      height: 0.8,
+      type: "Water",
+      abilities: ["Illuminate", " Natural Cure"],
     },
     {
-        name: 'Meowth',
-        nationalNumber: 052,
-        height: 0.4,
-        type: 'Normal',
-        abilities: ['Pickup', 'Technician']
+      name: "Meowth",
+      nationalNumber: 052,
+      height: 0.4,
+      type: "Normal",
+      abilities: ["Pickup", " Technician"],
     },
     {
-        name: 'Poliwag',
-        nationalNumber: 060,
-        height: 0.6,
-        type: 'Water',
-        abilities: ['Water Absorb', 'Damp']
+      name: "Poliwag",
+      nationalNumber: 060,
+      height: 0.6,
+      type: "Water",
+      abilities: ["Water Absorb", " Damp"],
     },
     {
-        name: 'Jigglypuff',
-        nationalNumber: 39,
-        height: 0.7,
-        type: ['Normal', 'Fairy'],
-        abilities: ['Pickup', 'Technician']
+      name: "Jigglypuff",
+      nationalNumber: 39,
+      height: 0.7,
+      type: ["Normal", " Fairy"],
+      abilities: ["Pickup", " Technician"],
     },
     {
-        name: 'Cubone',
-        nationalNumber: 104,
-        height: 0.4,
-        type: 'Ground',
-        abilities: ['Rock Head', 'Lightning Rod']
+      name: "Cubone",
+      nationalNumber: 104,
+      height: 0.4,
+      type: "Ground",
+      abilities: ["Rock Head", " Lightning Rod"],
     },
     {
-        name: 'Exeggutor',
-        nationalNumber: 103,
-        height: 2,
-        type: ['Grass', 'Psychic'],
-        abilities: 'Chlorophyll'
+      name: "Exeggutor",
+      nationalNumber: 103,
+      height: 2,
+      type: ["Grass", " Psychic"],
+      abilities: "Chlorophyll",
     },
     {
-        name: 'Mewtwo',
-        nationalNumber: 150,
-        height: 3,
-        type: 'Psychic',
-        abilities: 'Pressure'
-    }
-] 
+      name: "Mewtwo",
+      nationalNumber: 150,
+      height: 3,
+      type: "Psychic",
+      abilities: "Pressure",
+    },
+  ];
 
-// Prints name and height of each pokemon
-for (let i = 0; i < pokemonList.length; i++) {
-    document.write("<br>" + pokemonList[i].name + " - " + pokemonList[i].height + " " + 'meters');
-// Identifies tallest pokemon and prints "That's a big pokemon!" next to height
-    if (pokemonList[i].height > 2) {
-    document.write(' - ' + 'That\'s a big pokemon!');
-}
-};
+   function add(pokemon) {
+    return pokemonList.push(pokemon);
+  } 
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll,
+  };
+})();
+
+pokemonListRepository.add({
+  name: "Diglett",
+  nationalNumber: 50,
+  height: 0.2,
+  type: "Ground",
+  abilities: ["Sand Veil", " Area Trap"],
+});
+
+console.log(pokemonListRepository.getAll());
+
+pokemonListRepository.getAll().forEach(function (pokemon) {
+  if (pokemon.height > 2) {
+    document.write(
+      "<p><b>" +
+        "Name: " +
+        "</b>" +
+        pokemon.name +
+        "<br><b>" +
+        "Height: " +
+        "</b>" +
+        pokemon.height +
+        " " +
+        "meters -- whoa that's huge!" +
+        "<br><b>" +
+        "Type: " +
+        "</b>" +
+        pokemon.type +
+        "<br><b>" +
+        "Abilities: " +
+        "</b>" +
+        pokemon.abilities +
+        "</p>"
+    );
+  } else {
+    document.write(
+      "<p><b>" +
+        "Name: " +
+        "</b>" +
+        pokemon.name +
+        "<br><b>" +
+        "Height: " +
+        "</b>" +
+        pokemon.height +
+        " " +
+        "meters" +
+        "<br><b>" +
+        "Type: " +
+        "</b>" +
+        pokemon.type +
+        "<br><b>" +
+        "Abilities: " +
+        "</b>" +
+        pokemon.abilities +
+        "</p>"
+    );
+  }
+});
